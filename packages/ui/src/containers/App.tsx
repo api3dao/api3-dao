76b1de5a9a6358cecd10c0d3ea15660854744e33
dorg-hg <hector@dorg.tech>
2020-11-26 03:08:07 -0500

    Added MaterialUI to project

--- packages/ui/src/containers/App.tsx
+++ packages/ui/src/containers/App.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import {
   BrowserRouter,
 } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core";
 
 import { AppRouter }from "containers";
+import { Navbar } from "components";
+import { theme } from "styles/theme";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <AppRouter />
+        <ThemeProvider theme={theme}>
+          <Navbar />
+          <AppRouter />
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
--- packages/ui/src/containers/AppRouter.tsx
+++ packages/ui/src/containers/AppRouter.tsx
@@ -5,19 +5,20 @@ import {
 } from "react-router-dom";
 
 import { Dashboard } from "containers";
+import { Navbar } from "components";
 
 function AppRouter() {
   return (
-    <Switch>
-      /*
-      <Route path={"/about"}>
-        {"<About />"}
-      </Route>
-      */
-      <Route path={["/dashboard", "/"]}>
-        <Dashboard />
-      </Route>
-    </Switch>
+      <Switch>    
+        /*
+        <Route path={"/about"}>
+          {"<About />"}
+        </Route>
+        */
+        <Route path={["/dashboard", "/"]}>
+          <Dashboard />
+        </Route>
+      </Switch>
   );
 }
 
