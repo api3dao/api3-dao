0b0a9b61e8805e81afea3a69d0e9c35b121cc2a7
Kris <kris@dorg.tech>
2021-02-06 13:50:18 -0600

    updated test_StakeUtils.ts to prepare for merge

--- packages/poc-v2/test/test_StakeUtils.ts
+++ packages/poc-v2/test/test_StakeUtils.ts
@@ -13,6 +13,7 @@ describe('StakeUtils', () => {
   let token: Api3Token
   let pool: TestPool
   let ownerAccount: Api3Token
+  let stakers: TestPool[]
   let sum = BigNumber.from(0);
   let sumDeposited = BigNumber.from(0);
   let sumUnstaked = BigNumber.from(0);
@@ -26,6 +27,45 @@ describe('StakeUtils', () => {
     pool = (await api3PoolFactory.deploy(token.address)) as TestPool
     const signer0 = hre.waffle.provider.getSigner(0)
     ownerAccount = token.connect(signer0)
+    stakers = await Promise.all(accounts.map(async account => {
+      await ownerAccount.transfer(account, 100)
+      const signer = hre.waffle.provider.getSigner(account)
+      const holder = token.connect(signer)
+      await holder.approve(pool.address, 100)
+      return pool.connect(signer)
+    }))
+  })
+
+  it('stakes a deposit', async () => {
+    await stakers[1].deposit(accounts[1], 100, accounts[1])
+
+    const deposited = (await pool.users(accounts[1])).unstaked
+    expect(deposited).to.equal(100)
+
+    await stakers[1].stake(50)
+
+    const staked = await pool.balanceOf(accounts[1])
+    expect(staked).to.equal(50)
+    const unstaked = (await pool.users(accounts[1])).unstaked
+    expect(unstaked).to.equal(50)
+  })
+
+  it('deposits and stakes in one tx', async () => {
+    await stakers[2].depositAndStake(accounts[2], 100, accounts[2])
+
+    const staked = await pool.balanceOf(accounts[2])
+    expect(staked).to.equal(100)
+    const unstaked = (await pool.users(accounts[2])).unstaked
+    expect(unstaked).to.equal(0)
+    const unpooled = await token.balanceOf(accounts[2])
+    expect(unpooled).to.equal(0)
+  })
+
+  it('schedules unstake', async () => {
+    await stakers[1].scheduleUnstake(25)
+
+    const unstakeAmount = (await pool.users(accounts[1])).unstakeAmount
+    expect(unstakeAmount).to.equal(25)
   })
 
   testValues.map((testValue, index) => {
