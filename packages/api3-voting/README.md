0c8bf380fefe2eef1e105da87f67f80b8fcbe322
bbenligiray <bbenligiray@gmail.com>
2021-03-09 18:21:19 +0300

    Add API3 Voting app

---
+++ packages/api3-voting/README.md
@@ -0,0 +1,9 @@
+# API3 Voting App
+
+> A slightly customized version version of the [Aragon Voting app](https://github.com/aragon/aragon-apps/tree/631048d54b9cc71058abb8bd7c17f6738755d950/apps/voting)
+
+In addition to having all the functionality of the Aragon Voting app, the API3 Voting app has the following features:
+
+- Integrates to the [API3 pool](/packages/pool/README.md)
+- Does not allow users to create a new vote less than `EPOCH_LENGTH` (defined in the API3 pool) apart (immutably set as 1 week)
+- Does not allow users that have less than `proposalVotingPowerThreshold` (defined in the API3 pool) to create a new vote (governable, initial value 0.1%)
---
+++ packages/api3-voting/contracts/test/mocks/Api3PoolMock.sol
@@ -0,0 +1,59 @@
+pragma solidity 0.4.24;
+
+import "@aragon/minime/contracts/MiniMeToken.sol";
+
+
+contract Api3PoolMock is MiniMeToken {
+    constructor(
+        MiniMeTokenFactory _tokenFactory,
+        MiniMeToken _parentToken,
+        uint _parentSnapShotBlock,
+        string _tokenName,
+        uint8 _decimalUnits,
+        string _tokenSymbol,
+        bool _transfersEnabled
+        )
+        public
+        MiniMeToken(
+            _tokenFactory,
+            _parentToken,
+            _parentSnapShotBlock,
+            _tokenName,
+            _decimalUnits,
+            _tokenSymbol,
+            _transfersEnabled
+            )
+    {
+    }
+
+    uint256 private mockEPOCH_LENGTH = 0;
+    uint256 private mockProposalVotingPowerThreshold = 0;
+
+    function setEPOCH_LENGTH(uint256 _EPOCH_LENGTH)
+        external
+    {
+        mockEPOCH_LENGTH = _EPOCH_LENGTH;
+    }
+
+    function setProposalVotingPowerThreshold(uint256 _proposalVotingPowerThreshold)
+        external
+    {
+        mockProposalVotingPowerThreshold = _proposalVotingPowerThreshold;
+    }
+
+    function EPOCH_LENGTH()
+        external
+        view
+        returns(uint256)
+    {
+        return mockEPOCH_LENGTH;
+    }
+
+    function proposalVotingPowerThreshold()
+        external
+        view
+        returns(uint256)
+    {
+        return mockProposalVotingPowerThreshold;
+    }
+}
