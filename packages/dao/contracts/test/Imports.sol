0e88a3644a67f924d13a8bc88025718ff4b5c837
BritikovKI <31989062+BritikovKI@users.noreply.github.com>
2021-03-29 17:37:01 +0300

    Update Imports.sol

--- packages/dao/contracts/test/Imports.sol
+++ packages/dao/contracts/test/Imports.sol
@@ -1,7 +1,6 @@
 pragma solidity 0.4.24;
 
 
-//TODO: This is added to use this contract in tests, I think this is stupid, need to think ho to change it
 import "@aragon/id/contracts/FIFSResolvingRegistrar.sol";
 
 contract Imports {
