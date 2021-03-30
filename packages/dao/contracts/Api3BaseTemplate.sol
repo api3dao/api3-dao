d428b6e7bb92c38e80b3c43e0af43aaaa8ccb623
kinspotting <kinspotting@protonmail.com>
2021-03-30 13:55:19 +0200

    fix remove redundant functions

--- packages/dao/contracts/Api3BaseTemplate.sol
+++ packages/dao/contracts/Api3BaseTemplate.sol
@@ -11,7 +11,6 @@ import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
 
 import "@aragon/os/contracts/acl/ACL.sol";
 import "@aragon/os/contracts/apm/Repo.sol";
-import "@aragon/os/contracts/apm/APMNamehash.sol";
 import "@aragon/os/contracts/kernel/Kernel.sol";
 import "@aragon/os/contracts/lib/ens/ENS.sol";
 import "@aragon/os/contracts/lib/ens/PublicResolver.sol";
@@ -21,18 +20,9 @@ import "@aragon/os/contracts/common/Uint256Helpers.sol";
 import "@aragon/id/contracts/IFIFSResolvingRegistrar.sol";
 
 
-contract Api3BaseTemplate is APMNamehash, IsContract {
+contract Api3BaseTemplate is IsContract {
     using Uint256Helpers for uint256;
 
-    /* Hardcoded constant to save gas
-    * bytes32 constant internal AGENT_APP_ID = apmNamehash("agent");                  // agent.aragonpm.eth
-    * bytes32 constant internal VAULT_APP_ID = apmNamehash("vault");                  // vault.aragonpm.eth
-    * bytes32 constant internal VOTING_APP_ID = apmNamehash("voting");                // voting.aragonpm.eth
-    * bytes32 constant internal SURVEY_APP_ID = apmNamehash("survey");                // survey.aragonpm.eth
-    * bytes32 constant internal PAYROLL_APP_ID = apmNamehash("payroll");              // payroll.aragonpm.eth
-    * bytes32 constant internal FINANCE_APP_ID = apmNamehash("finance");              // finance.aragonpm.eth
-    * bytes32 constant internal TOKEN_MANAGER_APP_ID = apmNamehash("token-manager");  // token-manager.aragonpm.eth
-    */
     bytes32 constant internal AGENT_APP_ID = 0x9ac98dc5f995bf0211ed589ef022719d1487e5cb2bab505676f0d084c07cf89a;
     bytes32 constant internal VAULT_APP_ID = 0x7e852e0fcfce6551c13800f1e7476f982525c2b5277ba14b24339c68416336d1;
     bytes32 constant internal VOTING_APP_ID = 0x9fa3927f639745e587912d4b0fea7ef9013bf93fb907d29faeab57417ba6e1d4;
@@ -85,15 +75,6 @@ contract Api3BaseTemplate is APMNamehash, IsContract {
 
     /* ACL */
 
-    function _createPermissions(ACL _acl, address[] memory _grantees, address _app, bytes32 _permission, address _manager) internal {
-        _acl.createPermission(_grantees[0], _app, _permission, address(this));
-        for (uint256 i = 1; i < _grantees.length; i++) {
-            _acl.grantPermission(_grantees[i], _app, _permission);
-        }
-        _acl.revokePermission(address(this), _app, _permission);
-        _acl.setPermissionManager(_manager, _app, _permission);
-    }
-
     function _createPermissionForTemplate(ACL _acl, address _app, bytes32 _permission) internal {
         _acl.createPermission(address(this), _app, _permission, address(this));
     }
@@ -143,10 +124,6 @@ contract Api3BaseTemplate is APMNamehash, IsContract {
 
     /* VAULT */
 
-    function _installVaultApp(Kernel _dao) internal returns (Vault) {
-        bytes memory initializeData = abi.encodeWithSelector(Vault(0).initialize.selector);
-        return Vault(_installDefaultApp(_dao, VAULT_APP_ID, initializeData));
-    }
 
     function _createVaultPermissions(ACL _acl, Vault _vault, address _grantee, address _manager) internal {
         _acl.createPermission(_grantee, _vault, _vault.TRANSFER_ROLE(), _manager);
@@ -185,147 +162,6 @@ contract Api3BaseTemplate is APMNamehash, IsContract {
         _acl.createPermission(_createVotesGrantee, _voting, _voting.CREATE_VOTES_ROLE(), _manager);
     }
 
-    /* SURVEY */
-
-    function _installSurveyApp(Kernel _dao, MiniMeToken _token, uint64 _minParticipationPct, uint64 _surveyTime) internal returns (Survey) {
-        bytes memory initializeData = abi.encodeWithSelector(Survey(0).initialize.selector, _token, _minParticipationPct, _surveyTime);
-        return Survey(_installNonDefaultApp(_dao, SURVEY_APP_ID, initializeData));
-    }
-
-    function _createSurveyPermissions(ACL _acl, Survey _survey, address _grantee, address _manager) internal {
-        _acl.createPermission(_grantee, _survey, _survey.CREATE_SURVEYS_ROLE(), _manager);
-        _acl.createPermission(_grantee, _survey, _survey.MODIFY_PARTICIPATION_ROLE(), _manager);
-    }
-
-    /* PAYROLL */
-
-    function _installPayrollApp(
-        Kernel _dao,
-        Finance _finance,
-        address _denominationToken,
-        IFeed _priceFeed,
-        uint64 _rateExpiryTime
-    )
-        internal returns (Payroll)
-    {
-        bytes memory initializeData = abi.encodeWithSelector(
-            Payroll(0).initialize.selector,
-            _finance,
-            _denominationToken,
-            _priceFeed,
-            _rateExpiryTime
-        );
-        return Payroll(_installNonDefaultApp(_dao, PAYROLL_APP_ID, initializeData));
-    }
-
-    /**
-    * @dev Internal function to configure payroll permissions. Note that we allow defining different managers for
-    *      payroll since it may be useful to have one control the payroll settings (rate expiration, price feed,
-    *      and allowed tokens), and another one to control the employee functionality (bonuses, salaries,
-    *      reimbursements, employees, etc).
-    * @param _acl ACL instance being configured
-    * @param _acl Payroll app being configured
-    * @param _employeeManager Address that will receive permissions to handle employee payroll functionality
-    * @param _settingsManager Address that will receive permissions to manage payroll settings
-    * @param _permissionsManager Address that will be the ACL manager for the payroll permissions
-    */
-    function _createPayrollPermissions(
-        ACL _acl,
-        Payroll _payroll,
-        address _employeeManager,
-        address _settingsManager,
-        address _permissionsManager
-    )
-        internal
-    {
-        _acl.createPermission(_employeeManager, _payroll, _payroll.ADD_BONUS_ROLE(), _permissionsManager);
-        _acl.createPermission(_employeeManager, _payroll, _payroll.ADD_EMPLOYEE_ROLE(), _permissionsManager);
-        _acl.createPermission(_employeeManager, _payroll, _payroll.ADD_REIMBURSEMENT_ROLE(), _permissionsManager);
-        _acl.createPermission(_employeeManager, _payroll, _payroll.TERMINATE_EMPLOYEE_ROLE(), _permissionsManager);
-        _acl.createPermission(_employeeManager, _payroll, _payroll.SET_EMPLOYEE_SALARY_ROLE(), _permissionsManager);
-
-        _acl.createPermission(_settingsManager, _payroll, _payroll.MODIFY_PRICE_FEED_ROLE(), _permissionsManager);
-        _acl.createPermission(_settingsManager, _payroll, _payroll.MODIFY_RATE_EXPIRY_ROLE(), _permissionsManager);
-        _acl.createPermission(_settingsManager, _payroll, _payroll.MANAGE_ALLOWED_TOKENS_ROLE(), _permissionsManager);
-    }
-
-    function _unwrapPayrollSettings(
-        uint256[4] memory _payrollSettings
-    )
-        internal pure returns (address denominationToken, IFeed priceFeed, uint64 rateExpiryTime, address employeeManager)
-    {
-        denominationToken = _toAddress(_payrollSettings[0]);
-        priceFeed = IFeed(_toAddress(_payrollSettings[1]));
-        rateExpiryTime = _payrollSettings[2].toUint64();
-        employeeManager = _toAddress(_payrollSettings[3]);
-    }
-
-    /* FINANCE */
-
-    function _installFinanceApp(Kernel _dao, Vault _vault, uint64 _periodDuration) internal returns (Finance) {
-        bytes memory initializeData = abi.encodeWithSelector(Finance(0).initialize.selector, _vault, _periodDuration);
-        return Finance(_installNonDefaultApp(_dao, FINANCE_APP_ID, initializeData));
-    }
-
-    function _createFinancePermissions(ACL _acl, Finance _finance, address _grantee, address _manager) internal {
-        _acl.createPermission(_grantee, _finance, _finance.EXECUTE_PAYMENTS_ROLE(), _manager);
-        _acl.createPermission(_grantee, _finance, _finance.MANAGE_PAYMENTS_ROLE(), _manager);
-    }
-
-    function _createFinanceCreatePaymentsPermission(ACL _acl, Finance _finance, address _grantee, address _manager) internal {
-        _acl.createPermission(_grantee, _finance, _finance.CREATE_PAYMENTS_ROLE(), _manager);
-    }
-
-    function _grantCreatePaymentPermission(ACL _acl, Finance _finance, address _to) internal {
-        _acl.grantPermission(_to, _finance, _finance.CREATE_PAYMENTS_ROLE());
-    }
-
-    function _transferCreatePaymentManagerFromTemplate(ACL _acl, Finance _finance, address _manager) internal {
-        _acl.setPermissionManager(_manager, _finance, _finance.CREATE_PAYMENTS_ROLE());
-    }
-
-    /* TOKEN MANAGER */
-
-    function _installTokenManagerApp(
-        Kernel _dao,
-        MiniMeToken _token,
-        bool _transferable,
-        uint256 _maxAccountTokens
-    )
-        internal returns (TokenManager)
-    {
-        TokenManager tokenManager = TokenManager(_installNonDefaultApp(_dao, TOKEN_MANAGER_APP_ID));
-        _token.changeController(tokenManager);
-        tokenManager.initialize(_token, _transferable, _maxAccountTokens);
-        return tokenManager;
-    }
-
-    function _createTokenManagerPermissions(ACL _acl, TokenManager _tokenManager, address _grantee, address _manager) internal {
-        _acl.createPermission(_grantee, _tokenManager, _tokenManager.MINT_ROLE(), _manager);
-        _acl.createPermission(_grantee, _tokenManager, _tokenManager.BURN_ROLE(), _manager);
-    }
-
-    function _mintTokens(ACL _acl, TokenManager _tokenManager, address[] memory _holders, uint256[] memory _stakes) internal {
-        _createPermissionForTemplate(_acl, _tokenManager, _tokenManager.MINT_ROLE());
-        for (uint256 i = 0; i < _holders.length; i++) {
-            _tokenManager.mint(_holders[i], _stakes[i]);
-        }
-        _removePermissionFromTemplate(_acl, _tokenManager, _tokenManager.MINT_ROLE());
-    }
-
-    function _mintTokens(ACL _acl, TokenManager _tokenManager, address[] memory _holders, uint256 _stake) internal {
-        _createPermissionForTemplate(_acl, _tokenManager, _tokenManager.MINT_ROLE());
-        for (uint256 i = 0; i < _holders.length; i++) {
-            _tokenManager.mint(_holders[i], _stake);
-        }
-        _removePermissionFromTemplate(_acl, _tokenManager, _tokenManager.MINT_ROLE());
-    }
-
-    function _mintTokens(ACL _acl, TokenManager _tokenManager, address _holder, uint256 _stake) internal {
-        _createPermissionForTemplate(_acl, _tokenManager, _tokenManager.MINT_ROLE());
-        _tokenManager.mint(_holder, _stake);
-        _removePermissionFromTemplate(_acl, _tokenManager, _tokenManager.MINT_ROLE());
-    }
 
     /* EVM SCRIPTS */
 
@@ -363,19 +199,6 @@ contract Api3BaseTemplate is APMNamehash, IsContract {
     function _latestVersionAppBase(bytes32 _appId) internal view returns (address base) {
         Repo repo = Repo(PublicResolver(ens.resolver(_appId)).addr(_appId));
         (,base,) = repo.getLatest();
-    }
-
-    /* TOKEN */
-
-    function _createToken(string memory _name, string memory _symbol, uint8 _decimals) internal returns (MiniMeToken) {
-        require(address(miniMeFactory) != address(0), ERROR_MINIME_FACTORY_NOT_PROVIDED);
-        MiniMeToken token = miniMeFactory.createCloneToken(MiniMeToken(address(0)), 0, _name, _decimals, _symbol, true);
-        emit DeployToken(address(token));
-        return token;
-    }
-
-    function _ensureMiniMeFactoryIsValid(address _miniMeFactory) internal view {
-        require(isContract(address(_miniMeFactory)), ERROR_MINIME_FACTORY_NOT_CONTRACT);
     }
 
     /* IDS */
