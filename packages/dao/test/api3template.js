f85a3483b5fd4711d51a21b573b302a76725fa98
Konstantin <BritikovKI@Yandex.ru>
2021-04-03 23:22:45 +0300

    added pool

--- packages/dao/test/api3template.js
+++ packages/dao/test/api3template.js
@@ -2,10 +2,9 @@
 /*eslint no-undef: "error"*/
 
 // const { APP_IDS } = require('@aragon/templates-shared/helpers/apps')
-const { hash } = require('eth-ens-namehash');
 const { assertRole } = require('@aragon/templates-shared/helpers/assertRole')(web3);
 const { getEventArgument } = require('@aragon/test-helpers/events');
-const { getENS, getTemplateAddress } = require('@aragon/templates-shared/lib/ens')(web3, artifacts);
+const { getTemplateAddress } = require('@aragon/templates-shared/lib/ens')(web3, artifacts);
 const { getInstalledAppsById } = require('@aragon/templates-shared/helpers/events')(artifacts);
 
 const ACL = artifacts.require('ACL');
@@ -14,8 +13,8 @@ const Api3Voting = artifacts.require('Api3Voting');
 const Api3Template = artifacts.require('Api3Template');
 const Api3Pool = artifacts.require('Api3Pool');
 
-contract('Api3Template', ([_, deployer, tokenAddress, authorized]) => { // eslint-disable-line no-unused-vars
-  let api3Template, dao, acl, receipt1,receipt2, CREATE_VOTES_ROLE;
+contract('Api3Template', ([_, deployer, api3Pool, tokenAddress, authorized]) => { // eslint-disable-line no-unused-vars
+  let api3Template, dao, acl, receipt1,receipt2;
 
   const SUPPORT_1 = 80e16;
   const ACCEPTANCE_1 = 40e16;
@@ -30,9 +29,9 @@ contract('Api3Template', ([_, deployer, tokenAddress, authorized]) => { // eslin
   });
 
   before('create bare entity', async () => {
-
-    receipt1 = await api3Template.newInstance('api3-heavy', deployer, [SUPPORT_1, ACCEPTANCE_1, VOTING_DURATION_1], deployer, { from: deployer });
-    receipt2 = await api3Template.newInstance('api3-light', deployer, [SUPPORT_2, ACCEPTANCE_2, VOTING_DURATION_2], deployer, { from: deployer });
+    api3Pool = await Api3Pool.new(tokenAddress);
+    receipt1 = await api3Template.newInstance('api3-heavy', (api3Pool.address), [SUPPORT_1, ACCEPTANCE_1, VOTING_DURATION_1], deployer, { from: deployer });
+    receipt2 = await api3Template.newInstance('api3-light', (api3Pool.address), [SUPPORT_2, ACCEPTANCE_2, VOTING_DURATION_2], deployer, { from: deployer });
 
     dao = Kernel.at(getEventArgument(receipt1, 'DeployDao', 'dao'));
     acl = ACL.at(await dao.acl());
@@ -46,13 +45,16 @@ contract('Api3Template', ([_, deployer, tokenAddress, authorized]) => { // eslin
   });
 
   it('installs the requested application correctly', async () => {
-    const ens = await getENS();
     let installedApps = getInstalledAppsById(receipt1);
     assert.equal(installedApps.voting.length, 1, 'should have installed 1 voting app');
+    assert.equal(installedApps.agent.length, 1, 'should have installed 1 voting app');
     const voting_1 = Api3Voting.at(installedApps.voting[0]);
+    const agent_1 = Api3Voting.at(installedApps.agent[0]);
     installedApps = getInstalledAppsById(receipt2);
     assert.equal(installedApps.voting.length, 1, 'should have installed 1 voting app');
+    assert.equal(installedApps.agent.length, 1, 'should have installed 1 voting app');
     const voting_2 = Api3Voting.at(installedApps.voting[0]);
+    const agent_2 = Api3Voting.at(installedApps.agent[0]);
 
     assert.isTrue(await voting_1.hasInitialized(), 'voting not initialized');
     assert.equal((await voting_1.voteTime()).toString(), VOTING_DURATION_1);
@@ -64,11 +66,10 @@ contract('Api3Template', ([_, deployer, tokenAddress, authorized]) => { // eslin
     assert.equal((await voting_2.supportRequiredPct()).toString(), SUPPORT_2);
     assert.equal((await voting_2.minAcceptQuorumPct()).toString(), ACCEPTANCE_2);
 
-
-    assert.equal((await voting_1.api3Pool()), (await voting_2.api3Pool()));
+    assert.equal((await voting_1.api3Pool()), api3Pool.address);
 
     await assertRole(acl, voting_1, { address: deployer }, 'CREATE_VOTES_ROLE', { address: authorized })
 
-    const api3Pool = Api3Pool.new(await voting_1.api3Pool());
+    await api3Pool.setDaoApps(agent_1.address, agent_2.address, voting_1.address, voting_2.address);
   })
 });
