7e5135ffaaf62b449ffd4a17810af4f9450e9022
Konstantin <BritikovKI@Yandex.ru>
2021-05-10 18:12:15 +0300

    no fun and comments clarified

--- packages/dao/truffle.js
+++ packages/dao/truffle.js
@@ -1,16 +1,2 @@
 module.exports = require("@aragon/os/truffle-config");
 
-// module.exports = {
-//     "compilers": {
-//         "solc": {
-//             "version": "0.4.24",
-//                 "settings": {
-//                 "optimizer": {
-//                     "enabled": true,
-//                         "runs": 200
-//                 },
-//                 "evmVersion": "byzantium"
-//             }
-//         }
-//     }
-// };
