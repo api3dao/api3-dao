0bc9ee4a4a29828f4b034e1e03005f5cdb27f9e5
bbenligiray <bbenligiray@gmail.com>
2021-06-13 15:53:35 +0300

    Fix Husky

--- packages/pool/test/RewardUtils.sol.js
+++ packages/pool/test/RewardUtils.sol.js
@@ -12,7 +12,6 @@ beforeEach(async () => {
     deployer: accounts[0],
     agentAppPrimary: accounts[1],
     agentAppSecondary: accounts[2],
-
     votingAppPrimary: accounts[3],
     votingAppSecondary: accounts[4],
     claimsManager: accounts[5],
