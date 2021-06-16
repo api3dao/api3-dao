f2d5690d4b0c1aa169dc4e381de10227a542ae0b
bbenligiray <bbenligiray@gmail.com>
2021-06-16 13:18:25 +0300

    Improve DAO deployment scripts

--- packages/dao/scripts/deploy.js
+++ packages/dao/scripts/deploy.js
@@ -1,4 +1,5 @@
 /*global artifacts, web3, Promise*/
+const fs = require("fs");
 const { hash: namehash } = require("eth-ens-namehash");
 const { getNetworkName } = require("@aragon/templates-shared/lib/network")(
   web3
@@ -6,22 +7,17 @@ const { getNetworkName } = require("@aragon/templates-shared/lib/network")(
 const deployTemplate = require("@aragon/templates-shared/scripts/deploy-template");
 const { getEventArgument } = require("@aragon/test-helpers/events");
 
-// https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-const DAO_ID = `api3-template-${Math.random().toString(36).substring(7)}`;
-const CONTRACT_NAME = "Api3Template";
-const VOTE_NAME = "Api3Voting";
-
 const Api3Pool = artifacts.require("Api3Pool");
 const Api3Token = artifacts.require("Api3Token");
 const Convenience = artifacts.require("Convenience");
+const TimelockManager = artifacts.require("TimelockManager");
 
-const SUPPORT_1 = 50e16;
-const ACCEPTANCE_1 = 50e16;
-
-const SUPPORT_2 = 50e16;
-const ACCEPTANCE_2 = 15e16;
-
-const EPOCH_LENGTH = 7 * 24 * 60 * 60;
+const daoId = `api3-template-${Math.random().toString(36).substring(7)}`;
+const supportRequiredPct1 = 50e16;
+const minAcceptQuorumPct1 = 50e16;
+const supportRequiredPct2 = 50e16;
+const minAcceptQuorumPct2 = 15e16;
+const epochLength = 7 * 24 * 60 * 60;
 
 /**
  * Returns the address of the deployer
@@ -44,48 +40,54 @@ const getDeployer = async () => {
  * For more information about how to use this script see: https://github.com/api3dao/api3-dao/issues/217
  *
  * NOTE: When making changes to this script consult the changes with DAO dashboard developers
- * as they (implicitely) depend on this script.
+ * as they (implicitly) depend on this script.
  */
 module.exports = async (callback) => {
   try {
     const network = await getNetworkName();
-
     if (network === "mainnet") {
-      // TODO: We should use specific DAO_ID and do NOT create new token and pool instance
+      // TODO: We should use specific daoId and do NOT create new token and pool instance
       throw new Error("This script is not yet ready for mainnet deployment!");
     }
 
     const deployer = await getDeployer();
 
     const api3Token = await Api3Token.new(deployer, deployer);
-    // Set TimelockManager as deployer
-    const api3Pool = await Api3Pool.new(api3Token.address, deployer, EPOCH_LENGTH);
+    const timelockManager = await TimelockManager.new(
+      api3Token.address,
+      deployer
+    );
+    const api3Pool = await Api3Pool.new(
+      api3Token.address,
+      timelockManager.address,
+      epochLength
+    );
     const convenience = await Convenience.new(api3Pool.address);
     const template = await deployTemplate(
       web3,
       artifacts,
-      network === "rinkeby" || network === "mainnet" || network === "ropsten"
-        ? DAO_ID + ".open"
-        : DAO_ID,
-      CONTRACT_NAME,
+      network === "rinkeby" || network === "ropsten" ? daoId + ".open" : daoId,
+      "Api3Template",
       [
         { name: "agent", contractName: "Agent" },
         { name: "vault", contractName: "Vault" },
-        { name: "api3voting", contractName: VOTE_NAME },
+        { name: "api3voting", contractName: "Api3Voting" },
         { name: "survey", contractName: "Survey" },
         { name: "payroll", contractName: "Payroll" },
         { name: "finance", contractName: "Finance" },
         { name: "token-manager", contractName: "TokenManager" },
       ]
     );
-    const api3VotingAppId = network === "rinkeby" || network === "mainnet" || network === "ropsten"
+    const api3VotingAppId =
+      network === "rinkeby" || network === "mainnet" || network === "ropsten"
         ? namehash("api3voting.open.aragonpm.eth")
         : namehash("api3voting.aragonpm.eth");
+
     const tx = await template.newInstance(
-      DAO_ID,
+      daoId,
       api3Pool.address,
-      [SUPPORT_1, ACCEPTANCE_1],
-      [SUPPORT_2, ACCEPTANCE_2],
+      [supportRequiredPct1, minAcceptQuorumPct1],
+      [supportRequiredPct2, minAcceptQuorumPct2],
       api3VotingAppId
     );
     const primaryVoting = getEventArgument(
@@ -118,6 +120,7 @@ module.exports = async (callback) => {
 
     const deployedAddresses = {
       api3Token: api3Token.address,
+      timelockManager: timelockManager.address,
       api3Pool: api3Pool.address,
       convenience: convenience.address,
       votingAppPrimary: getEventArgument(
@@ -142,6 +145,10 @@ module.exports = async (callback) => {
       ).toString(),
     };
 
+    fs.writeFileSync(
+      `${network}.deployment.json`,
+      JSON.stringify(deployedAddresses, null, 2)
+    );
     console.log("\nDEPLOYED ADDRESSES:");
     console.log(JSON.stringify(deployedAddresses, null, 2));
   } catch (error) {
